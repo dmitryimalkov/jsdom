fd23dc958c13d5a8fe04146c5fda4e53dc2f1ba6
dmitryimalkov <dmitry.i.malkov@gmail.com>
2024-02-01 16:47:57 +0300

    all

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 //document.body.children[1].children[0].href = "https://google.com";
 let anchorElement = document.getElementById('external_link');
 anchorElement.href = "https://google.com";
+
+document.querySelector('');
